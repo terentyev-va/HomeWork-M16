ae928e9314d3f4849b666b58ace7ea3516690cb9
terentyev-va <terentyev.va@yandex.ru>
2022-09-13 21:38:59 +0300

    Initial Commit

---
+++ M16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16
+//
+//  Created by Вячеслав Терентьев on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
